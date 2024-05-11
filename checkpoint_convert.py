62c6f750a15061df6d33219c2cf044804f39a44d
Rui Cao <caorui0303@gmail.com>
2024-05-11 14:53:27 +0800

    update checkpoint convert script

--- checkpoint_convert.py
+++ checkpoint_convert.py
@@ -2,7 +2,7 @@ import torch
 import glob
 import os
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 checkpoint_root = '/media/gpuadmin/rcao/result/ignet'
 load_path = os.path.join(checkpoint_root, 'ignet_v0.8.2_backup')
 save_path = os.path.join(checkpoint_root, 'ignet_v0.8.2')
@@ -12,15 +12,7 @@ print(checkpoint_list)
 for checkpoint_path in checkpoint_list:
     checkpoint = torch.load(checkpoint_path, map_location='cuda:0')
     print(checkpoint.keys())
-    model_state_dict = checkpoint['model_state_dict']
-    print(model_state_dict.keys())
-    new_model_state_dict = {}
-    for key in model_state_dict.keys():
-        new_key = key.replace('module.', '')
-        new_model_state_dict[new_key] = model_state_dict[key]
-    checkpoint['model_state_dict'] = new_model_state_dict
     os.makedirs(os.path.join(save_path, camera), exist_ok=True)
     save_name = os.path.join(save_path, camera, os.path.basename(checkpoint_path))
-    torch.save(checkpoint, save_name)
+    torch.save(checkpoint['model_state_dict'], save_name)
     print(f'Save checkpoint to {save_name}')
-# torch.save(checkpoint['model_state_dict'], 'checkpoint.tar')
